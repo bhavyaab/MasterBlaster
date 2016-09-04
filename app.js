77f152f215abb29ada01ae85e8a34d8af6b95a1d
Bhavya Bharti <bhavyab@outlook.com>
2016-09-04 14:23:39 -0700

    going to master

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 // generate Data object
 function Person(name, ranking, phNum, email, wana_play, wana_share_loc ){
-    this.name = name;
-    this.ranking = ranking;
-    this.phNum = phNum;
-    this.email = email;
-    this.wana_play = wana_play;
-    this.wana_share_loc = wana_share_loc;
-  };
-var data = [];
+  this.name = name;
+  this.ranking = ranking;
+  this.phNum = phNum;
+  this.email = email;
+  this.wana_play = wana_play;
+  this.wana_share_loc = wana_share_loc;
+};
+var data = []; //data of all people.
 
 function sign_up(event){
   event.preventDefault();
+  localStorage.clear;
   var name = event.target.name.value;
   var ranking = 0;
   var phNum = event.target.phNum.value;
@@ -21,5 +22,17 @@ function sign_up(event){
   form.reset();
 }
 //Function for error catching
- 
-document.getElementById('form').addEventListener('submit', sign_up);
+
+document.getElementById('form').addEventListener('submit', sign_up); //eventListner is been called by submit btn.//
+
+//save data generated from the form
+if (localStorage.list) {
+  var list = localStorage.list.split(',');
+} else {
+  var list = [];
+};
+function saveInfo(){
+list.push(new Person(name, ranking, phNum, email, wana_play, wana_share_loc));
+
+
+}
--- data.js
+++ data.js
@@ -10,11 +10,36 @@ var data = [
     wana_share_loc: 'No',
   },
 
+   {
+    name = 'Craig Notion';
+    ranking = 3;
+    phNum = 9892898922;
+    email = 'craig@email.com';
+    wana_play = 'Yes';
+    wana_share_loc = 'No';
+   }
+    {
+    name = 'Erin Lee';
+    ranking = 2;
+    phNum = 9892345522;
+    email = 'erin@email.com';
+    wana_play = 'No';
+    wana_share_loc = 'No';
+  };
+   {
+   name = 'Michel Molinari';
+   ranking = 1;
+   phNum = 9892345232;
+   email = 'mmolinari@email.com';
+   wana_play = 'Yes';
+   wana_share_loc = 'No';
+ };
   {
-     name: 'Craig Camble',
-     ranking: 40,
-      num: 999-999-8888,
-     email: 'abc@efg.com',
-     wana_play: 'Yes',
-     wana_share_loc: 'No',
-  }]
+  name = 'Bhavya Bharti';
+  ranking = 99;
+  phNum = 9234345522;
+  email = 'bhavyab@email.com';
+  wana_play = 'No';
+  wana_share_loc = 'No';
+};
+  ]
