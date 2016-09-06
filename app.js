50bc921839d27dcf00797304126c4b8683748976
Bhavya Bharti <bhavyab@outlook.com>
2016-09-06 09:35:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -27,17 +27,4 @@ div.appendChild(ul);
 ul = document.createElement('ul');
 ul.innerHTML = '<li> Name: ' + myData[numThree].name + '</li> <li> Score: ' + myData[numThree].score + '</li>';
 div.appendChild(ul);
-function Dog(name,breed,legs) {
-  this.name = name;
-this.breek = breed;
-this.legs = 4;
-this.isAGoodDog = true;
-}
-};
-Dog.prototype.says = function(bark) {
-  console.log(Dog.says);
-}
-var dog1 = new Dog('Parker', 'English Shepherd',legs) {
-console.log(this.says);
-}
 
