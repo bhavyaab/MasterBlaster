239b513bcc693e4affc47221dd92ff68443d3539
Bhavya Bharti <bhavyab@outlook.com>
2016-09-06 08:55:01 -0700

    app java is displaying data on home page

---
+++ js/data.js
@@ -0,0 +1,63 @@
+var myData = [
+  {
+    userName: 'BrianN',
+    password: 'pwd',
+    name: 'Brian Nations',
+    score: 230,
+    phNum: 9999998888,
+    email: 'abc@efg.com',
+    wanaPlay: 'yes',
+    wanaShareLoc: 'no'
+
+  },
+  {
+    userName: 'RileyR',
+    password: 'pwd',
+    name: 'Riley Rose',
+    score: 24,
+    phNum: 9990009898,
+    email: 'riley@gin.com',
+    wanaPlay: 'yes',
+    wanaShareLoc: 'yes'
+  },
+  {
+    userName: 'BileyR',
+    password: 'pwd',
+    name: 'Biley Rose',
+    score: 289,
+    phNum: 9993239898,
+    email: 'biley@gin.com',
+    wanaPlay: 'no',
+    wanaShareLoc: 'no'
+  },
+  {
+    userName: 'Ramank',
+    password: 'pwd',
+    name: 'Raman Kumar',
+    score: 12,
+    phNum: 9993232398,
+    email: 'rfgy@gin.com',
+    wanaPlay: 'no',
+    wanaShareLoc: 'no'
+  },
+  {
+    userName: 'Erin',
+    password: 'pwd',
+    name: 'Erin Lee',
+    score: 279,
+    phNum: 9234567898,
+    email: 'erinl@gin.com',
+    wanaPlay: 'no',
+    wanaShareLoc: 'yes'
+  },
+  {
+    userName: 'Bhavyab',
+    password: 'pwd',
+    name: 'Bhavya Bharti',
+    score: 65,
+    phNum: 9334239898,
+    email: 'bhavya@gin.com',
+    wanaPlay: 'yes',
+    wanaShareLoc: 'no'
+  }
+];
---
+++ js/form.js
@@ -0,0 +1,44 @@
+// generate Data object
+function Person(username, password, name, ranking, phNum, email, wana_play, wana_share_loc ){
+  this.userName = userName;
+  this.password = password;
+  this.name = name;
+  this.ranking = ranking;
+  this.phNum = phNum;
+  this.email = email;
+  this.wana_play = wana_play;
+  this.wana_share_loc = wana_share_loc;
+};
+var data = []; //data of all people.
+//Function for error catching
+
+document.getElementById('form').addEventListener('submit', sign_up); //eventListner is been called by submit btn.//
+function sign_up(event){
+  event.preventDefault();
+  localStorage.clear;
+  var userName = event.target.userName.value;
+  var password = event.target.password.value;
+  var name = event.target.name.value;
+  var ranking = 0;
+  var phNum = event.target.phNum.value;
+  var email = event.target.email.value;
+  var wana_play = event.target.wana_play.value;
+  var wana_share_loc = event.target.wana_share_loc.value;
+  for(i = 0; i < data.length; i++){
+    if(userName = data[i].username){
+      prompt('This userName is taken, choose different.');
+    };
+  };
+  data.push(new Person(userName, password, name, ranking, phNum, email, wana_play, wana_share_loc ));
+  form.reset();
+}
+
+//save data generated from the form
+// if (localStorage.list) {
+//   var list = localStorage.list.split(',');
+// } else {
+//   var list = [];
+// };
+// function saveInfo(){
+//   list.push(new Person(userName, password, name, ranking, phNum, email, wana_play, wana_share_loc));
+// }
