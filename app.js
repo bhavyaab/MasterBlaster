7768c90b613f6fcf4436b0f72e675a9afcad7e5a
Bhavya Bharti <bhavyab@outlook.com>
2016-09-06 12:11:07 -0700

    profile markup

--- app.js
+++ app.js
@@ -13,17 +13,11 @@ var ranking = scores.slice();
 //copy the scores to another array
 ranking = ranking.sort(compareNumbers);
 //find the top three scorer
-var numOne = scores.indexOf(ranking[ranking.length - 1]);
-var numTwo = scores.indexOf(ranking[ranking.length - 2]);
-var numThree = scores.indexOf(ranking[ranking.length - 3]);
 //DOM
 var div = document.getElementById('rank');
-var ul = document.createElement('ul');
-ul.innerHTML = '<li> Name: ' + myData[numOne].name + '</li> <li> Score: ' + myData[numOne].score + '</li>';
-div.appendChild(ul);
-ul = document.createElement('ul');
-ul.innerHTML = '<li> Name: ' + myData[numTwo].name + '</li> <li> Score: ' + myData[numTwo].score + '</li>';
-div.appendChild(ul);
-ul = document.createElement('ul');
-ul.innerHTML = '<li> Name: ' + myData[numThree].name + '</li> <li> Score: ' + myData[numThree].score + '</li>';
-div.appendChild(ul);
+for(var r = 0; r < 3; r++){
+  var numOne = scores.indexOf(ranking[ranking.length - r - 1]);
+  var ul = document.createElement('ul');
+  ul.innerHTML = '<li> Name: ' + myData[numOne].name + '</li> <li> Score: ' + myData[numOne].score + '</li>';
+  div.appendChild(ul);
+}
