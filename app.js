98b2ea255d95d25dd6c41a8c9adfda7e5718e779
Bhavya Bharti <bhavyab@outlook.com>
2016-09-06 09:32:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+<<<<<<< HEAD
+//ranking of each user
+var scores = [];
+for(i = 0; i < myData.length; i++){
+  scores.push(myData[i].score);
+};
+//short array ranking by order off smaller to higher
+
+function compareNumbers(a, b){
+  return a - b;
+};
+//comapir the array ranking and find the heighest score
+var ranking = scores.slice();
+//copy the scores to another array
+ranking = ranking.sort(compareNumbers);
+//find the top three scorer
+var numOne = scores.indexOf(ranking[ranking.length - 1]);
+var numTwo = scores.indexOf(ranking[ranking.length - 2]);
+var numThree = scores.indexOf(ranking[ranking.length - 3]);
+//DOM
+var div = document.getElementById('rank');
+var ul = document.createElement('ul');
+ul.innerHTML = '<li> Name: ' + myData[numOne].name + '</li> <li> Score: ' + myData[numOne].score + '</li>';
+div.appendChild(ul);
+ul = document.createElement('ul');
+ul.innerHTML = '<li> Name: ' + myData[numTwo].name + '</li> <li> Score: ' + myData[numTwo].score + '</li>';
+div.appendChild(ul);
+ul = document.createElement('ul');
+ul.innerHTML = '<li> Name: ' + myData[numThree].name + '</li> <li> Score: ' + myData[numThree].score + '</li>';
+div.appendChild(ul);
+=======
 function Dog(name,breed,legs) {
   this.name = name;
 this.breek = breed;
@@ -11,3 +42,4 @@ Dog.prototype.says = function(bark) {
 var dog1 = new Dog('Parker', 'English Shepherd',legs) {
 console.log(this.says);
 }
+>>>>>>> 8c44b920d01c64dde34d22955ea9686ec73eb8ba
