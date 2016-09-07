bc8cddfc022c22100643f5b49558e11f7c5d8d41
Bhavya Bharti <bhavyab@outlook.com>
2016-09-07 11:50:58 -0700

    login and display is working

--- js/profile.js
+++ js/profile.js
@@ -3,54 +3,47 @@ var signIn = document.getElementById('login_form');
 var info = document.getElementById('info');
 signIn.style.display = 'block';
 info.style.display = 'none';
+//declear function to show up info
+function showInfo(j) {
+  signIn.style.display = 'none';
+  info.style.display = 'block';
+  var img = document.createElement('img');
+  img.setAttribute('class', 'pic');//create image element
+  img.setAttribute('src', myData[j].image);// append src to img tag
+  var ul = document.createElement('ul');
+  ul.setAttribute('class', 'info');
+  var myName = document.createElement('li');
+  myName.innerHTML = myData[j].name;
+  var myInfo = document.createElement('li');
+  myInfo.innerHTML = 'Score: ' + myData[j].score + '</li><li> Wins: ' + myData[j].wins + '</li><li>Losses: ' + myData[j].losses;
+  ul.appendChild(myName);
+  ul.appendChild(myInfo);
+  info.appendChild(img);
+  info.appendChild(ul);
+  document.body.appendChild(info);
+  return;
+};
 //add userName and passWord input area form DOM
 //check if username and password is right
 function check(event){
   event.preventDefault();
-  console.log('event check', event);
-  alert('this event fired');
   var username = event.target.userName.value;
   var password = event.target.passWord.value;
   for(var i = 0; i < myData.length; i++){
     if(username === myData[i].userName && password === myData[i].passWord){
-         var j = i;
-         return j;
-    }else{
-      signIn.style.display = 'block';
-      info.style.display = 'none';
-      alert('username or password is worng!!');
-      return;
-    };
-    new ShowInfo(j);
+      console.log('userName =', myData[i].userName, 'passWord =', myData[i].passWord);
+      var j = i;
+    }
+    // else{
+    //   signIn.style.display = 'block';
+    //   info.style.display = 'none';
+    //   alert('username or password is worng!!');
+    //   return;
+    // };
   }
+  showInfo(j);
 }
-//declear function to show up info
-function ShowInfo(n){
- console.log(myData[n].userName);
- console.log(myData[n].passWord);
- signIn.style.display = 'none';
- info.style.display = 'block';
- var img = document.createElement('img');
- img.setAttribute('class', 'pic');//create image element
- img.setAttribute('src', myData[n].image);// append src to img tag
- var ul = document.createElement('ul');
- ul.setAttribute('class', 'info');
- var myName = document.createElement('li');
- myName.innerHTML = myData[n].name;
- var myInfo = document.createElement('li');
- myInfo.innerHTML = 'Score: ' + myData[n].score + '</li><li> Wins: ' + myData[n].wins + '</li><li>Losses: ' + myData[n].losses;
- ul.innerHTML = 'myName';
- ul.innerHTML = 'myInfo';
- var button = document.createElement('button');
- button.setAttribute('type', 'submit');
- button.setAttribute('id', 'update');
- button.innerHTML = 'update';
- info.innerHTML = 'img';
- info.innerHTML = 'ul';
- info.innerHTML = 'button';
- document.body.appendChild(info);
- return;
-}
+
 //infoPage(3);
 document.getElementById('login_form').addEventListener('submit', check);
 
